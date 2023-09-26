92dc7a3f7343d6eede1daf801d0bb3c9bff76af5
Peng Jiang <16434640+pengjiang80@users.noreply.github.com>
2023-09-26 09:16:31 +0800

    Update HelloController.java

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
 	@GetMapping("/")
 	public String index() {
-		return "<div style='text-align: center; font-weight: bold; font-size: 24px;'>Greetings from Walrus! TADA!</div>";
+		return "<div style='display: flex; align-items: center; justify-content: center; height: 100vh;'><div style='text-align: center; font-weight: bold; font-size: 48px;'>Greetings from Walrus&Appilot! TADA!</div></div>";
 	}
 }
